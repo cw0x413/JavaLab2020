f7d401e4593585a18b7f229b65c56545b44bbf0c
sonuishaq67 <sonuishaq67@gmail.com>
2020-12-21 16:59:09 +0530

    uhh start of day5?

---
+++ day5/Circle.java
@@ -0,0 +1,32 @@
+package day5;
+public class Circle {
+    float radius;
+    Circle(float radius){
+        this.radius=radius;
+    }
+    float area(){
+        return 3.14f*radius*radius;
+    }
+}
+
+class Sector extends Circle{
+    float radian;
+    Sector(float radian,float radius){
+        super(radius);
+        this.radian=radian;
+    }
+    float area(){
+        return 0.5f*radian*radius*radius;
+    }
+}
+
+class Segment extends Circle{
+    float length;
+    Segment(float length,float radius){
+        super(radius);
+        this.length=length;
+    }
+    float area(){
+        return  (float) ((radius*radius)*((radius-length)/length)-(radius-length)*Math.pow((2*radius*length-(length*length)),0.5));
+    }
+}

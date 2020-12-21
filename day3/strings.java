96735836f173e95aefeefbabb53dc1e27a9e7636
ishaq shaik <ishaqshaik084@gmail.com>
2020-12-21 09:59:59 +0530

    made menu for 6

--- day3/strings.java
+++ day3/strings.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class strings {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+    void extractSubString(){
         // extract a substring from string n , m begin , end index
         int n, m;
         System.out.println("extract a substring from string n , m begin , end index\n");
@@ -15,6 +15,8 @@ public class strings {
         String subString = string.substring(n, n + m);
         System.out.println(subString);
         scanner.nextLine();
+    }
+    void countOccurence(){
         // read a text and count occurences of particular word
         System.out.println("read a text and count occurences of particular word\n");
         System.out.println("Enter the text");
@@ -28,6 +30,9 @@ public class strings {
                 count++;
         System.out.println("The word was found " + count + " times");
         scanner.nextLine();
+        
+    }
+    void replaceSubString(){
         // replace substring in given string
         System.out.println("replace substring in given string\n");
         System.out.println("Enter the text ");
@@ -37,6 +42,8 @@ public class strings {
         String replacement = scanner.next();
         String output = target.replace(toReplace, replacement);
         System.out.println(output);
+    }
+    void sortString(){
         // reARRANGE string in alphabetical order
         System.out.println("reARRANGE string in alphabetical order");
         System.out.println("Enter the string to sort");
@@ -45,17 +52,45 @@ public class strings {
         Arrays.sort(CharArr);
         String sortedString = new String(CharArr);
         System.out.println(sortedString);
+    }
+    void compareStrings(){
         // COMPARE TWO STRINGS IGNORING CASE
         System.out.println("COMPARE TWO STRINGS IGNORING CASE");
         System.out.println("Enter two strings");
         String toCompString = scanner.next(), toCompString2 = scanner.next();
         String result = toCompString.compareToIgnoreCase(toCompString2) == 0 ? "same" : "not same";
         System.out.println("Strings are " + result);
+    }
+    void concatStrings(){
         // concat
         System.out.println("to concat two strings");
         System.out.println("Enter two strings");
         String cat = scanner.next(), concatString = scanner.next();
         cat = cat.concat(concatString);
         System.out.println("Concatted strings are " + cat);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        strings stringmanip=new strings();
+        int CASE=10;
+        while(CASE!=7){
+            System.out.println("Enter the case \n1)extract sub string\n2)count Occurence\n3)replace sub string\n4)sort String\n5)compare strings\n6)concat Strings\n7)exit");
+            CASE=sc.nextInt();
+            if(CASE==1)
+                stringmanip.extractSubString();
+            else if (CASE==2)
+                stringmanip.countOccurence();
+            else if (CASE==3)
+                stringmanip.replaceSubString();
+            else if (CASE==4)
+                stringmanip.sortString();
+            else if (CASE==5)
+                stringmanip.compareStrings();
+            else if (CASE==6)
+                stringmanip.concatStrings();
+        }
+        System.out.println("Exitted");
+        sc.close();
+        System.exit(0);;   
     }
 }
